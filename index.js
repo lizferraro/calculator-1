b0e2fa468a2996472ba61ab3c6118f5afb07f511
Liz Ferraro <elizabeth.k.ferraro@gmail.com>
2022-07-10 21:03:19 -0400

    final solution

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-let num1 = 8
-let num2 = 2
-document.getElementById("num1-el").textContent = num1
-document.getElementById("num2-el").textContent = num2
-
 // Create four functions: add(), subtract(), divide(), multiply()
 // Call the correct function when the user clicks on one of the buttons
 // Perform the given calculation using num1 and num2
@@ -11,18 +6,31 @@ document.getElementById("num2-el").textContent = num2
 // E.g. if the user clicks on the "Plus" button, you should render
 // "Sum: 10" (since 8 + 2 = 10) inside the paragraph with id="sum-el"
 
+let num1 = 8
+let num2 = 2
+document.getElementById("num1-el").textContent = num1
+document.getElementById("num2-el").textContent = num2
+
+let sumEl = document.getElementById("sum-el")
+
 function add() {
-    console.log("button clicked")
+    let result = num1 + num2
+    sumEl.textContent = "Sum: " + result
 }
 
 function subtract() {
-    console.log("button clicked")
+    let result = num1 - num2
+    sumEl.textContent = "Sum: " + result
 }
 
 function multiply() {
-    console.log("button clicked")
+    let result = num1 * num2
+    sumEl.textContent = "Sum: " + result
 }
 
 function divide() {
-    console.log("button clicked")
+    let result = num1 / num2
+    sumEl.textContent = "Sum: " + result
 }
+
+// Return to this challenge later and add number buttons (or an input option for user to type numbers) so user can enter their own math problems
